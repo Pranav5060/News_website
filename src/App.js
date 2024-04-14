90f5f114f8c492aa0ca1b66416441630a3a5878e
Pranav5060 <pranav.gupta2020@vitbhopal.ac.in>
2024-04-14 20:43:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import News from './Components/News';
+
+function App() {
+  return (
+    <div>
+      <News/>
+    </div>
+  );
+}
+
+export default App;
